6be8a1da71c3d876fc25668b2fa9bb8374996725
Thorsten Schaeff <thorsten@stripe.com>
2019-04-25 11:04:12 +0100

    Utility example initial commit.

---
+++ utility/server/node/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const {resolve} = require('path');
+const envPath = resolve('.env');
+const env = require('dotenv').config({path: envPath});
+const stripe = require('stripe')(env.parsed.STRIPE_SECRET_KEY);
+
+app.use(express.static('./client'));
+app.use(express.json());
+
+// Render the checkout page
+app.get('/', (request, response) => {
+  const path = resolve('./client/index.html');
+  response.sendFile(path);
+});
+
+// A webhook to receive events sent from Stripe
+// You can listen for specific events
+//
+app.post('/webhook', async (req, res) => {
+  // Check if webhook signing is configured.
+  if (env.parsed.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        env.parsed.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // we can retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+});
+
+// Start server
+const listener = app.listen(process.env.PORT || 3000, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
